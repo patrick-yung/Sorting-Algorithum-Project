2ab7ca52aa225a8e8d4922ab151756ebef4c6bc1
Yung Pak Hong Patrick <79505817+patrick-yung@users.noreply.github.com>
2021-09-04 11:05:15 +0800

    Update Bottom-Up Merge Sort.py

--- MergeSort/Python/Bottom-Up Merge Sort.py
+++ MergeSort/Python/Bottom-Up Merge Sort.py
@@ -1,12 +1,16 @@
-import time
+import time #To measure time#
 
+#Main mergeSort function#
 def mergesortA(A,start, middle, end):
+
+  #seperating the array#
   left=middle-start+1
   right=end-middle;
 
   leftarray=[]
   rightarray=[]
 
+  #Create Copys to both array#
   tmp=0
   tmp1=0
   while(tmp<left):
@@ -20,7 +24,7 @@ def mergesortA(A,start, middle, end):
   index1=0
   index2=0
   index3=start
-
+  #Perform comparions untill one array is empty#
   while index1<left and index2<right:
     if(leftarray[index1]<rightarray[index2]):
       A[index3]=leftarray[index1]
@@ -31,6 +35,7 @@ def mergesortA(A,start, middle, end):
       index2+=1
       index3+=1
 
+  #Copy the reminding array back into orginal array#
   while index1 < left:
     A[index3]=leftarray[index1]
     index1+=1
@@ -42,7 +47,7 @@ def mergesortA(A,start, middle, end):
     index3+=1
   return
   
-  
+#Structure of the merge sort#  
 def mergesort(A,start,end):
   C=1
   while(C<=end):
@@ -52,14 +57,24 @@ def mergesort(A,start,end):
       B+=2*C
     C+=C
   
+
+ #The data set# 
 arr=[573, -187, -400, -356, -100, 440, 355, -116, -9, 229, -5, 969, -179, 894, -664, 205, -753, -132, 271, 441, 976, -931, -67, -247, -382, 889, 496, -487, -998, -154, -573, 864, 962, -572, -676, -534, 478, -691, -555, -386, -868, -766, -660, 197, 742, 933, -943, 887, -201, -898, 971, -607, -260, 40, -148, 353, -449, 93, 52, -548, 174, 250, 96, -174, -231, 233, 286, 70, -372, -960, 358, 22, -981, -554, -562, 558, 746, 490, -621, -822, -86, 696, -642, -93, -873, 411, 619, -926, 780, -203, 420, -553, -523, -447, -68, 786, 645, 674, -383, 448
 ]
+
+#Start stopwatch#
 start = time.time_ns()
 n=len(arr)
 mergesort(arr,0,n-1)
+
+#Stop the stopwatch#
 end = time.time_ns()-start
+
+#Print array to confirm#
 print(end)
 print("ns")
 print("The Sorted-Array is: ", arr) 
+
+
 
 
